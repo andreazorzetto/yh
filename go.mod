684610d7367bc96c4163e2e9f9b2d490ad953942
Andrea Zorzetto <andrea.zorzetto@ecs-digital.co.uk>
2020-10-18 10:21:16 +0100

    merging PR#13

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/andreazorzetto/yh
+
+go 1.15
+
+require github.com/logrusorgru/aurora v2.0.3+incompatible
--- main.go
+++ main.go
@@ -1,125 +1,32 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"log"
 	"os"
+
+	"github.com/andreazorzetto/yh/highlight"
 )
 
 const version = "0.3.3"
 
 func main() {
-	// Service vars
-	foundChompingIndicator := false
-	indentationSpacesBeforeComment := 0
+	log.SetFlags(0) // disable timestamp of log package
 
 	// Checking the args, someone out there might need help
 	checkArgs(os.Args)
 
-	// Warm-up the engine
-	scanner := bufio.NewScanner(os.Stdin)
-
-	// Get the juice
-	for scanner.Scan() {
-
-		if scanner.Text() == "EOF" {
-			break
-		}
-
-		// Check for errors during Stdin read
-		err := scanner.Err()
-
-		if err != nil {
-			println("Error:", err)
-			os.Exit(1)
-		}
-
-		// Drink the juice
-		l := yamlLine{raw: scanner.Text()}
-
-		if (foundChompingIndicator == true) && (l.indentationSpaces() > indentationSpacesBeforeComment) {
-			// Found multiline comment or configmap, not treated as YAML at all
-
-			printMultiline(l)
-
-		} else if l.isKeyValue() {
-			// This is a valid YAML key: value line. Key and value are returned in l
-
-			if l.isComment() {
-				// This line is a comment
-
-				printComment(l)
-
-			} else if l.valueIsNumberOrIP() {
-				// The value is a number or an IP address x.x.x.x
-
-				printKeyNumberOrIP(l)
-
-			} else if l.valueIsBoolean() {
-				// The value is boolean true or false
-
-				printKeyBool(l)
-
-			} else {
-				// The is a normal key/value line
-
-				printKeyValue(l)
-			}
-
-			if l.valueContainsChompingIndicator() {
-				// This line contains a chomping indicator, sign of a possible multiline text coming next
-
-				// Setting flag for next execution
-				foundChompingIndicator = true
-
-				// Saving current number of indentation spaces
-				indentationSpacesBeforeComment = l.indentationSpaces()
-
-			} else {
-				// Resetting multiline flag
-				foundChompingIndicator = false
-			}
-
-		} else if !l.isEmptyLine() {
-			// This is not a YAML key: value line and is not empty
-
-			if l.isUrl() {
-				// The value is boolean true or false
-
-				printUrl(l)
-
-			} else if l.isComment() {
-				// This line is a comment
-
-				printComment(l)
-
-			} else if l.isElementOfList() {
-				// This line is an element of a list
-
-				printListElement(l)
-
-			} else {
-				// This line is not valid YAML
-
-				printInvalidLine(l)
-			}
-
-			foundChompingIndicator = false
-
-		} else if l.isEmptyLine() {
-			// This is an empty line
-
-			fmt.Println(l.raw)
-		}
-
+	h, err := highlight.Highlight(os.Stdin)
+	if err != nil {
+		log.Fatalln(err)
 	}
 
+	fmt.Print(h)
 }
 
 // Check args if passed
 // Show help
 func checkArgs(a []string) {
-
 	if len(a) >= 2 {
 		// Someone's looking for...
 
